4145b578a3f5bbfd5ec91389fe20ad4712d88da1
Romain Rabouan <romainrabouan@macbook-air-de-romain.home>
2016-06-19 15:40:02 +0200

    Added Hello World :)

---
+++ Transports/HelloWorldViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HelloWorldViewController.swift
+//  Transports
+//
+//  Created by Romain Rabouan on 19/06/16.
+//  Copyright © 2016 RomainRabouan. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world!")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
